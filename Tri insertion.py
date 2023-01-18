9a68178616df32825113509a50de8553b974c736
A-naima <naimaabdou148@gmail.com>
2023-01-18 10:12:36 +0100

    1.2

--- Tri insertion.py
+++ Tri insertion.py
@@ -1,12 +1,7 @@
 import numpy as np
-x=int(input("Seed:"))
-y=int(input("Nombre de génération:"))
-np.random.seed(0)
-for i in range (y):
-   s=np.random.choice(range(1,45),5,replace=False)
-   s=list(s)
 
-
+np.random.seed(int(input("Seed:")))
+y = int(input("Nombre de génération:"))
 
 def insertionSort(arr):
     n = len (arr)
@@ -16,7 +11,11 @@ def insertionSort(arr):
         while j >= 0 and key_item < arr[j] :
             arr[j + 1] = arr[j]
             j -=1
-            arr[j + 1] = key_item
-            return arr
+        arr[j + 1] = key_item
+    return arr
 
-print(s)
+for i in range(y):
+    s = np.random.choice(range(1, 45), 5, replace=False)
+    s = list(s)
+    print("Série générée :", s)
+    print("Série triée :", insertionSort(s))
