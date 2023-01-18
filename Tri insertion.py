7a2affd96bbeec5be14fc0bd001b6418a3f5904b
A-naima <naimaabdou148@gmail.com>
2023-01-18 10:06:45 +0100

    finale

--- Tri insertion.py
+++ Tri insertion.py
@@ -1,5 +1,10 @@
-
-
+import numpy as np
+x=int(input("Seed:"))
+y=int(input("Nombre de génération:"))
+np.random.seed(0)
+for i in range (y):
+   s=np.random.choice(range(1,45),5,replace=False)
+   s=list(s)
 
 
 
@@ -13,3 +18,5 @@ def insertionSort(arr):
             j -=1
             arr[j + 1] = key_item
             return arr
+
+print(s)
--- tri cocktail.py
+++ tri cocktail.py
@@ -1,12 +1,5 @@
-#from numpy.random import radint
+#from numpy.random import radintimport numpy as np
 import numpy as np
-x=int(input("Seed:"))
-y=int(input("Nombre de génération:"))
-np.random.seed(0)
-for i in range (y):
-   s=np.random.choice(range(1,45),5,replace=False)
-   s=list(s)
-    print(s)
 
 def cocktailsort(arr):
     al = True
@@ -17,7 +10,7 @@ def cocktailsort(arr):
         al = False
         for i in range (debut,fin):
             if (arr[i] > arr[i+1]):
-                arr[i], arr[i + 1],arr[i]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 al = True
         if (al == False):
             break
@@ -25,15 +18,16 @@ def cocktailsort(arr):
         fin = fin-1
         for i in range(fin-1, debut-1,-1):
             if (arr[i] > arr[i+1]):
-                arr[i], arr[i + 1],arr[i + 1],arr[i]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 al = True
         debut = debut + 1
     return arr
 
-#if __name__=='__main__':
-   # x = int(input("Entrez le nombre de tirage:"))
-    #for i in range(x):
-       # arr = np.random.choice(range(1,45),5,replace=False)
-        #arr = list(arr)
-        #print(("Le tirage est :", arr))
-       # print("Le tirage triée:", cocktailsort(arr))
+if __name__=='__main__':
+    x = int(input("Entrez le nombre de tirage:"))
+    np.random.seed(int(input("Seed:")))
+    for i in range(x):
+        arr = np.random.choice(range(1,45),5,replace=False)
+        arr = list(arr)
+        print("Le tirage est :", arr)
+        print("Le tirage triée:", cocktailsort(arr))
